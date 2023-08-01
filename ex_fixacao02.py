58705946752ce84533c3ea51fc19eedc3050c946
ART0059179 <fernandofalcaoscot.cpv@gmail.com>
2023-08-01 11:16:16 -0300

    Exercício de fixação de número 2

---
+++ ex_fixacao02.py
@@ -0,0 +1,5 @@
+bill = float(input("Insira o valor que é cobrado na comanda do restaurante: R$"))
+
+acrescimo = 0.1
+
+print("Valor com o acréscimo de 10% do garçom que lhe atendeu: R$", bill + (acrescimo * bill))
